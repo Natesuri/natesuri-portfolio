6b16aed2122a4387569669e3b41d68be95e0d372
Natesuri <natesuri@gmail.com>
2019-03-18 15:53:23 -0400

    Renders Temp Homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link, Redirect } from 'react-router-dom'
+import Home from './pages/components/Home.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Route exact path='/' render={() => <Home/>} />
       </React.Fragment>
     );
   }
---
+++ src/pages/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import '../pages.scss'
+
+const Home  = () => (
+<h1>This is the Homepage</h1>
+)
+
+export default Home

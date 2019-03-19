62c3d20a0079953622e7bd499c490207a0aadd67
Natesuri <natesuri@gmail.com>
2019-03-19 18:45:37 -0400

    Adds and imports images

--- src/pages/components/Projects.js
+++ src/pages/components/Projects.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+// images
+import npf from '../../images/npf.png'
+import fb from '../../images/fb.png'
+import garden from '../../images/garden.png'
+import tictactoe from '../../images/tic tac toe.png'
+
+// styles
 import '../pages.scss'
 
 const Projects  = () => (

aeaab9f1efcf4df451ef2972203d9715ac14ebf4
Natesuri <natesuri@gmail.com>
2019-03-19 13:03:23 -0400

    Adds Routes for page Components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Route, Link, Redirect } from 'react-router-dom'
 import Header from './header/Header.js'
 import Home from './pages/components/Home.js'
+import Projects from './pages/components/Projects.js'
+import Skills from './pages/components/Skills.js'
+import Contact from './pages/components/Contact.js'
 import './App.scss';
 
 class App extends Component {
@@ -10,6 +13,9 @@ class App extends Component {
       <React.Fragment>
         <Header path='/' />
         <Route exact path='/' render={() => <Home/>} />
+        <Route exact path='/projects' render={() => <Projects/>} />
+        <Route exact path='/skills' render={() => <Skills/>} />
+        <Route exact path='/contact' render={() => <Contact/>} />
       </React.Fragment>
     );
   }

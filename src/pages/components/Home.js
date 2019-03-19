194c54870baa2065f984dfdd3facb1024775c9ff
Natesuri <natesuri@gmail.com>
2019-03-19 15:59:21 -0400

    Converts Home class to React Component
    Currently still a stateless component. Needed to change the setup to hopefully add some animation via set timeout function.

--- src/pages/components/Home.js
+++ src/pages/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 // images
@@ -7,32 +7,40 @@ import profile from '../../images/suri.jpg'
 // styles
 import '../pages.scss'
 
-const Home  = () => (
-  <section id="about" className='view active'>
-          <div className='container'>
-            <div className='double'>
-              <div className=''>
-                <img className='profile' src={profile} alt=''/>
-              </div>
-              <div className='about'>
-                <h2>Howdy! I'm <span className='emphasis-1'>Nate Suri</span>.</h2>
-                <p>
-                  I’m a <span className='emphasis-2'>storyteller</span> at heart with a mission to share the story of innovators and difference makers in my community and the world.
-                  My experience in both filmmaking and programming gives me a unique perspective on how to create <span className='emphasis-3'>engaging and creative</span> web experiences.
-                </p>
-                <p>
-                  During my time at General Assembly’s Web Development Immersive bootcamp,
-                  I built three full stack applications, learning and leveraging technologies
-                  like JavaScript, Ruby on Rails, Express, React, HTML, CSS, Git, Sass, MongoDB
-                  and PostgreSQL to create those applications.
-                </p>
-                <p>
-                  In addition to my web developer experience, I have 10 years of video editing experience, working professionally in documentary post-production for the last three years.
-                </p>
-              </div>
+class Home extends Component {
+
+
+  // componentDidMount () {}
+
+  render () {
+    return (
+      <section id="about" className='view active'>
+        <div className='container'>
+          <div className='double'>
+            <div className=''>
+              <img className='profile' src={profile} alt=''/>
+            </div>
+            <div className='about'>
+              <h2>Howdy! I'm <span className={emphasisOne}>Nate Suri</span>.</h2>
+              <p>
+                I’m a <span className={emphasisTwo}>storyteller</span> at heart with a mission to share the story of innovators and difference makers in my community and the world.
+                My experience in both filmmaking and programming gives me a unique perspective on how to create <span className={emphasisThree}>engaging and creative</span> web experiences.
+              </p>
+              <p>
+                During my time at General Assembly’s Web Development Immersive bootcamp,
+                I built three full stack applications, learning and leveraging technologies
+                like JavaScript, Ruby on Rails, Express, React, HTML, CSS, Git, Sass, MongoDB
+                and PostgreSQL to create those applications.
+              </p>
+              <p>
+                In addition to my web developer experience, I have 10 years of video editing experience, working professionally in documentary post-production for the last three years.
+              </p>
             </div>
           </div>
-        </section>
-)
+        </div>
+      </section>
+    )
+  }
+}
 
 export default Home

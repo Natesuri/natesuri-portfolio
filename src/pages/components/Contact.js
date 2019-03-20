112538f13b41e20078b65a9a62cc9c2669f625aa
Natesuri <natesuri@gmail.com>
2019-03-19 20:14:42 -0400

    Contact Page Structure and Styling

--- src/pages/components/Contact.js
+++ src/pages/components/Contact.js
@@ -4,7 +4,19 @@ import { Link } from 'react-router-dom'
 import '../pages.scss'
 
 const Contact  = () => (
-<h1>This is the Contact Page</h1>
+  <section id="contact" class='view'>
+        <div className='container'>
+          <div className='contact-blurb'>
+            <h2>Contact</h2>
+            <p>I'd love to chat. Please reach out!</p>
+            <a className='contact email' href='mailto:natesuri@gmail.com?Subject=Hey%20Nate'>natesuri [at] gmail [dot] com</a>
+            <div className='w-100'></div>
+            <a className='contact github' href='https://github.com/Natesuri'>natesuri [on] github</a>
+            <div className='w-100'></div>
+            <a className='contact linkedIn' href='https://www.linkedin.com/in/natesuri/'>natesuri [Linked] In</a>
+          </div>
+        </div>
+      </section>
 )
 
 export default Contact

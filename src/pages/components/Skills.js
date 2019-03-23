1f46bb84df5f37cff20ef4195eb2b73e1f88d2ff
Natesuri <natesuri@gmail.com>
2019-03-23 17:12:52 -0400

    Adds to Skills Pages
    Biggest change from old site is using flex to
    wrap the icons, making it more responsive.

--- src/pages/components/Skills.js
+++ src/pages/components/Skills.js
@@ -3,8 +3,31 @@ import { Link } from 'react-router-dom'
 
 import '../pages.scss'
 
+// would love to add a function that adds the class "colored" to an <i> when it's hovered over.
+
 const Skills  = () => (
-<h1>This is the Skills Page</h1>
+  <section className='view'>
+        <div className='container'>
+          <div className='skills'>
+            <h2>Skills</h2>
+            <div className='skills-list' id='mySkills'>
+              <i className="devicon-html5-plain-wordmark"></i>
+              <i className="devicon-css3-plain-wordmark"></i>
+              <i className="devicon-javascript-plain"></i>
+              <i className="devicon-express-original-wordmark"></i>
+              <i className="devicon-ruby-plain-wordmark"></i>
+              <i className="devicon-rails-plain-wordmark"></i>
+              <i className="devicon-nodejs-plain-wordmark"></i>
+              <i className="devicon-mongodb-plain-wordmark"></i>
+              <i className="devicon-postgresql-plain-wordmark"></i>
+              <i className="devicon-sass-original"></i>
+              <i className="devicon-bootstrap-plain-wordmark"></i>
+              <i className="devicon-jquery-plain-wordmark"></i>
+              <i className="devicon-react-original"></i>
+            </div>
+          </div>
+        </div>
+      </section>
 )
 
 export default Skills
